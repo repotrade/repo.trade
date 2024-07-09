f7f803048c6a31c9af6254df80a60cba67d9acaf
Fabian Schuh <mail@xeroc.org>
2024-07-09 12:43:52 +0200

    feat: htmx, subscription to api

---
+++ build-es.mjs
@@ -0,0 +1,22 @@
+import * as esbuild from 'esbuild'
+
+const config = {
+    entryPoints: ['src/assets/es/main.js'],
+    outdir: 'docs/assets/ js',
+    bundle: true,
+    minify: true,
+    plugins: [],
+    sourcemap: true,
+}
+function success() {
+    console.log('Bundling successful!');
+}
+
+function error(msg) {
+    console.error('Bundling failed:', msg);
+}
+
+await esbuild.build(config)
+    .then(success)
+    .catch(error);
+
---
+++ src/assets/es/main.js
@@ -0,0 +1,3 @@
+window.htmx = require('htmx.org');
+import('htmx.org/dist/ext/client-side-templates');
+window.Handlebars = require("handlebars");
