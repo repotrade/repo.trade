685ef0386ee36ec10f2e252c986de8480a59f388
Fabian Schuh <mail@xeroc.org>
2024-07-22 10:11:29 +0200

    feat: whitepaper

--- src/assets/es/main.js
+++ src/assets/es/main.js
@@ -1,3 +1,24 @@
 window.htmx = require('htmx.org');
 import('htmx.org/dist/ext/client-side-templates');
 window.Handlebars = require("handlebars");
+
+// Collapse
+window.addEventListener('click', (ev) => {
+    const elm = ev.target.nextSibling;
+    if (elm && elm.classList.contains("collapse")) {
+        collapse(elm, 'toggle');
+    }
+}, false);
+const fnmap = {
+    'toggle': 'toggle',
+    'show': 'add',
+    'hide': 'remove'
+};
+const collapse = (target, cmd) => {
+    const targets = Array.from(document.querySelectorAll(".collapse"));
+    targets.forEach(t => {
+        if (t != target)
+            t.classList[fnmap["hide"]]('show');
+    });
+    target.classList[fnmap[cmd]]('show');
+}
